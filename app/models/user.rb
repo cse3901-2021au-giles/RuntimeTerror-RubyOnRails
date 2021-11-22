939c4ac59db184424381971429558bb413ae385c
Kevin <wang.12470@osu.edu>
2021-11-22 14:33:18 -0500

    Comment out faulty check for user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   belongs_to :course
 
-  if role == 1
-    has_many :CreateTeamsUsersJoin
-    has_many :teams, through: :CreateTeamsUsersJoin
-  end
+  # Broken. role 1 means student
+  #if role == 1
+  has_many :CreateTeamsUsersJoin
+  has_many :teams, through: :CreateTeamsUsersJoin
+  #end
 end
