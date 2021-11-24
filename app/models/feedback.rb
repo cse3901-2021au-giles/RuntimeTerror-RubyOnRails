179065306a7f7a8c43c64c50bd68fec5085a7df7
Kevin <wang.12470@osu.edu>
2021-11-24 18:57:15 -0500

    Add uniqueness validations for combinations of attributes of feedback, team, and user

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,10 @@
 class Feedback < ApplicationRecord
+
+  # Clarify foreign keys giveuser and receiveuser point to user
   belongs_to :giveuser, class_name: 'User'
   belongs_to :receiveuser, class_name: 'User'
+
+  # Ensure the combination of fid, team_id, course_id, giveuser_id, and receiveuser_id is unique
+  validates_uniqueness_of :fid, scope: %i[team_id course_id giveuser_id receiveuser_id]
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,23 @@
 class User < ApplicationRecord
-  #belongs_to :course, optional: true
+  # Relation with Courses
   has_many :CoursesUsers
   has_many :courses, :through => :CoursesUsers, :source => :course
 
-  # Broken. role 1 means student
-  #if role == 1
+  # Relation with Teams
   has_many :TeamsUsers
   has_many :teams, :through => :TeamsUsers, :source => :team
-  #end
 
   # Relation with feedback
   has_many :giveuser_feedbacks, class_name: 'Feedback', foreign_key: 'giveuser_id'
   has_many :receiveuser_feedbacks, class_name: 'Feedback', foreign_key: 'receiveuser_id'
+
+  # Ensure different users don't use the same email
+  validates_uniqueness_of :email
   
   before_save { email.downcase! }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :fname, presence: true, length { maximum: 25 }
-  validates :lname, presence: true, length { maximum: 25 }
+  validates :fname, presence: true, length: { maximum: 25 }
+  validates :lname, presence: true, length: { maximum: 25 }
 
 end
