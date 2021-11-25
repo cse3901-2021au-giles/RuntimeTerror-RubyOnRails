552a3eb34ab9a9f7c50d4ad03b2ed0dcfa9d8438
Kevin <wang.12470@osu.edu>
2021-11-25 12:53:21 -0500

    Modified sessions new to be routed as new home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def new
+    render html: "Home"
   end
   
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get 'sessions/adminView'
+  root 'sessions#new'
   resources :users
   resources :feedbacks
   resources :teams
   resources :courses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root 'application#hello'
+  #root 'application#hello'
   # get '/login', to: 'sessions#new'
 end
