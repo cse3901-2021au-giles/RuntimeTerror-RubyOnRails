276b0272fd779ae81656a7ec05b6c1ebd8d1534f
Kevin <wang.12470@osu.edu>
2021-12-01 21:13:37 -0500

    Implement seeds.rb

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -11,5 +11,5 @@ class Feedback < ApplicationRecord
   validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
 
   # Ensure the combination of fid, team_id, course_id, giveuser_id, and receiveuser_id is unique
-  validates_uniqueness_of :fid, scope: %i[team_id course_id giveuser_id receiveuser_id]
+  validates_uniqueness_of :fid, scope: %i[team_id checkpoint_id giveuser_id receiveuser_id]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,61 +8,63 @@
 
 ## Current seeds:
 
-#   courses = Course.create([
-#             {id: "1", course_name: "CSE3901", class_code: "asdf1234", admin_code: "qwer1234"}
-#   ])
+courses = Course.create([
+          {id: "1", course_name: "CSE3901", class_code: "cse3901", admin_code: "asdf1234"},
+          {id: "2", course_name: "CSE3902", class_code: "cse3902", admin_code: "qwer1234"},
+])
 
-#   users = User.create ([
-#           {id: "1", email: "hu.2056@osu.edu", fname: "Matthew", lname: "Hu", role: "1", password:"asdf1"},
-#           {id: "2", email: "garry.19@osu.edu", fname: "Jack", lname: "Garry", role: "1", password:"asdf2"},
-#           {id: "3", email: "wang.12470@osu.edu", fname: "Kevin", lname: "Wang", role: "1", password:"asdf3"},
-#           {id: "4", email: "wilcox.331@osu.edu", fname: "Luke", lname: "Wilcox", role: "1", password:"asdf4"},
-#           {id: "5", email: "giles.1@osu.edu", fname: "Charlie", lname: "Giles", role: "0", password:"asdf5"},
-#           {id: "6", email: "vanhoose.3@osu.edu", fname: "Justin", lname: "Vanhoose", role: "1", password:"asdf6"}
-#   ])
+users = User.create ([
+        {id: "1", email: "hu.2056@osu.edu", fname: "Matthew", lname: "Hu", role: "1", password:"password", password_confirmation:"password"},
+        {id: "2", email: "garry.19@osu.edu", fname: "Jack", lname: "Garry", role: "1", password:"password", password_confirmation:"password"},
+        {id: "3", email: "wang.12470@osu.edu", fname: "Kevin", lname: "Wang", role: "1", password:"password", password_confirmation:"password"},
+        {id: "4", email: "wilcox.331@osu.edu", fname: "Luke", lname: "Wilcox", role: "1", password:"password", password_confirmation:"password"},
+        {id: "5", email: "giles.1@osu.edu", fname: "Charlie", lname: "Giles", role: "0", password:"password", password_confirmation:"password"},
+        {id: "6", email: "vanhoose.3@osu.edu", fname: "Justin", lname: "Vanhoose", role: "1", password:"password", password_confirmation:"password"},
+])
 
-#   teams = Team.create ([
-#           {id: "1", tid: "1", course_id: "1", team_name:"RuntimeTerror"},
-#           {id: "2", tid: "2", course_id: "1", team_name:"Team Peach"},
-#           {id: "3", tid: "3", course_id: "1", team_name:"Team Luigi"}
-#   ])
+teams = Team.create ([
+        {id: "1", tid: "1", course_id: "1", team_name:"RuntimeTerror"},
+        {id: "2", tid: "2", course_id: "1", team_name:"Team Peach"},
+        {id: "3", tid: "3", course_id: "1", team_name:"Team Luigi"},
+])
 
-#   coursesUsers = CoursesUser.create([
-#                  {course_id: "1", user_id: "1"},
-#                  {course_id: "1", user_id: "2"},
-#                  {course_id: "1", user_id: "3"},
-#                  {course_id: "1", user_id: "4"},
-#                  {course_id: "1", user_id: "5"},
-#                  {course_id: "1", user_id: "6"},
-#   ])
+coursesUsers = CoursesUser.create([
+               {course_id: "1", user_id: "1"},
+               {course_id: "1", user_id: "2"},
+               {course_id: "1", user_id: "3"},
+               {course_id: "1", user_id: "4"},
+               {course_id: "1", user_id: "5"},
+               {course_id: "1", user_id: "6"},
+               {course_id: "2", user_id: "3"}
+])
 
-#   teamsUsers = TeamsUser.create([
-#                  {team_id: "1", user_id: "1"},
-#                  {team_id: "1", user_id: "2"},
-#                  {team_id: "1", user_id: "3"},
-#                  {team_id: "1", user_id: "4"},
-#                  {team_id: "2", user_id: "3"},
-#                  {team_id: "2", user_id: "6"},
-#   ])
+teamsUsers = TeamsUser.create([
+               {team_id: "1", user_id: "1"},
+               {team_id: "1", user_id: "2"},
+               {team_id: "1", user_id: "3"},
+               {team_id: "1", user_id: "4"},
+               {team_id: "2", user_id: "3"},
+               {team_id: "2", user_id: "6"},
+])
 
-#   feedbacks = Feedback.create([
-#                   {id: "1", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "1", receiveuser_id: "2", done: "false", body: "text"},
-#                   {id: "2", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "1", receiveuser_id: "3", done: "false", body: "text2"},
-#                   {id: "3", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "1", receiveuser_id: "4", done: "false", body: "text3"},
-#                   {id: "4", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "2", receiveuser_id: "1", done: "false", body: "text4"},
-#                   {id: "5", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "2", receiveuser_id: "3", done: "false", body: "text5"},
-#                   {id: "6", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "2", receiveuser_id: "4", done: "false", body: "text6"},
-#                   {id: "7", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "3", receiveuser_id: "1", done: "false", body: "text7"},
-#                   {id: "8", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "3", receiveuser_id: "2", done: "false", body: "text8"},
-#                   {id: "9", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "3", receiveuser_id: "4", done: "false", body: "text9"},
-#                   {id: "10", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "4", receiveuser_id: "1", done: "false", body: "text10"},
-#                   {id: "11", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "4", receiveuser_id: "2", done: "false", body: "text11"},
-#                   {id: "12", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "4", receiveuser_id: "3", done: "false", body: "text12"},
-#                   {id: "13", fid: "1", team_id: "2", checkpoint_id: "2", giveuser_id: "3", receiveuser_id: "6", done: "false", body: "text13"},
-#   ])
+feedbacks = Feedback.create([
+                {id: "1", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "1", receiveuser_id: "2", done: "false", body: "text"},
+                {id: "2", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "1", receiveuser_id: "3", done: "false", body: "text2"},
+                {id: "3", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "1", receiveuser_id: "4", done: "false", body: "text3"},
+                {id: "4", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "2", receiveuser_id: "1", done: "false", body: "text4"},
+                {id: "5", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "2", receiveuser_id: "3", done: "false", body: "text5"},
+                {id: "6", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "2", receiveuser_id: "4", done: "false", body: "text6"},
+                {id: "7", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "3", receiveuser_id: "1", done: "false", body: "text7"},
+                {id: "8", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "3", receiveuser_id: "2", done: "false", body: "text8"},
+                {id: "9", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "3", receiveuser_id: "4", done: "false", body: "text9"},
+                {id: "10", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "4", receiveuser_id: "1", done: "false", body: "text10"},
+                {id: "11", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "4", receiveuser_id: "2", done: "false", body: "text11"},
+                {id: "12", fid: "1", team_id: "1", checkpoint_id: "1", giveuser_id: "4", receiveuser_id: "3", done: "false", body: "text12"},
+                {id: "13", fid: "1", team_id: "2", checkpoint_id: "2", giveuser_id: "3", receiveuser_id: "6", done: "false", body: "text13"},
+])
 
-#  Checkpoint.create([
-#                  {id:"1", team_id:"1", checkpoint_name:"Project 2", due_date:"2021-11-30 11:59:59.00000 +000"},
-#                  {id:"2", team_id:"2", checkpoint_name:"Tech Project", due_date:"2021-12-08 11:59:59.00000 +000"},
-#                  {id:"3", team_id:"3", checkpoint_name:"Tech Project", due_date:"2021-12-08 11:59:59.00000 +000"}
-#   ])
+checkpoints = Checkpoint.create([
+               {id:"1", team_id:"1", checkpoint_name:"Project 2", due_date:"2021-11-30 11:59:59.00000 +000"},
+               {id:"2", team_id:"2", checkpoint_name:"Tech Project", due_date:"2021-12-08 11:59:59.00000 +000"},
+               {id:"3", team_id:"3", checkpoint_name:"Tech Project", due_date:"2021-12-08 11:59:59.00000 +000"},
+])
