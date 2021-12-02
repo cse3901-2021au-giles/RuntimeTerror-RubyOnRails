f73ff09ee05edda64784a5e27f01ce515b0c3f63
Matthew Hu <matt.hu017@gmail.com>
2021-12-02 00:32:29 -0500

    Display details relating to the user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   def set_current_user
     if session[:user_id]
       Current.user = User.find_by(id: session[:user_id])
+      Current.courses = Course.joins(:users).where(users: Current.user.id)
     end
   end
 
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,14 @@
 class MainController < ApplicationController
     def index
-      if session[:user_id]
-        @user = User.find_by(id: session[:user_id])
+      if Current.user
+        if Current.user.role == 1 # Student
+          Current.teams = Team.joins(:users).where(users: Current.user.id)
+          Current.checkpoints = Checkpoint.joins(Current.teams)
+          #Current.give_feedbacks = Feedback.joins(:giveuser).where(user: Current.user.id)
+          #Current.receive_feedbacks = Feedback.joins(:receiveuser).where(user: Current.user.id)
+        else # Professor
+          foo = 1
+        end
       end
     end
 end
