acaf7adf8b3bc7e5b4505e5d00e18071d9528193
Kevin <wang.12470@osu.edu>
2021-12-06 20:56:14 -0500

    Implement model tests for Courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,9 @@ class Course < ApplicationRecord
   # many to many relationship with user
   has_many :CoursesUsers
   has_many :users, :through => :CoursesUsers, :source => :user
+
+  validates :class_code, presence: true
+  validates :admin_code, presence: true
   
   # Ensure there is a unique class code
   validates_uniqueness_of :class_code
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid course" do
+    course = courses(:one)
+    assert course.valid?
+  end
+
+  test "valid course 2" do
+    course = courses(:two)
+    assert course.valid?
+  end
+
+  test "invalid without class_code" do
+    course = Course.new(id: "3", course_name:"noClassCode", admin_code:"asdfd")
+    refute course.valid?, "course is valid without a class code"
+  end
+
+  test "invalid without admin_code" do
+    course = Course.new(id: "3", course_name:"noClassCode", class_code:"asdfd")
+    refute course.valid?, "course is valid without an admin code"
+  end
+
+  test "invalid with duplicate class code" do
+    course = Course.new(id: "3", course_name:"DupeClassCode", class_code: "MyString2", admin_code:"MyString4")
+    refute course.valid?, "course is valid with a duplicate class code"
+  end
+
+  test "invalid with duplicate admin code" do
+    course = Course.new(id: "3", course_name: "DupeAdminCode", class_code: "MyString234", admin_code:"MyString2")
+    refute course.valid?, "course is valid with a duplicate admin code"
+  end
 end
