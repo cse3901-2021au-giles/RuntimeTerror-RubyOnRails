b36cbbf0fb1f705068042260df8289a5bebb3486
Jack Garry <jackgarry4@gmail.com>
2021-12-06 20:07:33 -0500

    Initial testing changes

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -2,47 +2,56 @@ require "test_helper"
 
 class CoursesControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @student = User.create! ({id: "1", email: "hu.2056@osu.edu", fname: "Matthew", lname: "Hu", role: "1", password:"password", password_confirmation:"password"})
+    @admin = users(:two)
     @course = courses(:one)
   end
 
-  test "should get index" do
-    get courses_url
+
+  test "should get user course page" do
+    post login_path(email: @student.email, password: @student.password)
+    get view_courses_url
     assert_response :success
   end
 
-  test "should get new" do
-    get new_course_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get courses_url
+  #   assert_response :success
+  # end
 
-  test "should create course" do
-    assert_difference('Course.count') do
-      post courses_url, params: { course: { admin_code: @course.admin_code, class_code: @course.class_code, course_name: @course.course_name } }
-    end
+  # test "should get new" do
+  #   get new_course_url
+  #   assert_response :success
+  # end
 
-    assert_redirected_to course_url(Course.last)
-  end
+  # test "should create course" do
+  #   assert_difference('Course.count') do
+  #     post registrations_new_paths, params: { course: { admin_code: @course.admin_code, class_code: @course.class_code, course_name: @course.course_name } }
+  #   end
 
-  test "should show course" do
-    get course_url(@course)
-    assert_response :success
-  end
+  #   assert_redirected_to course_url(Course.last)
+  # end
 
-  test "should get edit" do
-    get edit_course_url(@course)
-    assert_response :success
-  end
+  # test "should show course" do
+  #   get view_course_details_path(@course)
+  #   assert_response :success
+  # end
 
-  test "should update course" do
-    patch course_url(@course), params: { course: { admin_code: @course.admin_code, class_code: @course.class_code, course_name: @course.course_name } }
-    assert_redirected_to course_url(@course)
-  end
+  # test "should get edit" do
+  #   get edit_course_url(@course)
+  #   assert_response :success
+  # end
 
-  test "should destroy course" do
-    assert_difference('Course.count', -1) do
-      delete course_url(@course)
-    end
+  # test "should update course" do
+  #   patch course_url(@course), params: { course: { admin_code: @course.admin_code, class_code: @course.class_code, course_name: @course.course_name } }
+  #   assert_redirected_to course_url(@course)
+  # end
 
-    assert_redirected_to courses_url
-  end
+  # test "should destroy course" do
+  #   assert_difference('Course.count', -1) do
+  #     delete course_url(@course)
+  #   end
+
+  #   assert_redirected_to courses_url
+  # end
 end
